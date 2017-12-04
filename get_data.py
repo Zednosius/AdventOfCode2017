e731aca03263056dafed3029850437af68f1d35c
Erik Söderberg <Zednosius@gmail.com>
2017-12-04 20:12:32 +0100

    Split data processing into functions

--- get_data.py
+++ get_data.py
@@ -1,3 +1,14 @@
+
+def process_line(line, cast=str):
+    return map(cast,map(str.strip, line.split()))
+
+def process_data(unprocessed_data, cast=str):
+    data = []
+    for line in unprocessed_data.splitlines():
+        data.append(list(process_line(line, cast)))
+    return data
+
+
 def get_data_from_file(filename, cast=str):
     """Returns data from filename in matrix format if there is more than one line.
     each element will be casted with the cast provided.
@@ -5,5 +16,8 @@ def get_data_from_file(filename, cast=str):
     data = []
     with open(filename, "r") as f:
         for line in f:
-            data.append(list(map(cast,map(str.strip, line.split()))))
+            data.append(list(process_line(line, cast))) 
     return data
+
+def get_data(day_num):
+    return get_data_from_file("day{}.txt".format(day_num))

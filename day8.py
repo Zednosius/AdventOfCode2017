9b8c2f605aa1dcc937e40bc74e6505c9716989e8
Erik Söderberg <Zednosius@gmail.com>
2017-12-08 10:14:48 +0100

    Add solution to day8 part 1

---
+++ day8.py
@@ -0,0 +1,43 @@
+import re
+from collections import defaultdict
+
+
+
+
+
+var = "(\w+)"
+op ="(inc|dec)"
+iff = "if"
+compre = "(<|>|<=|>=|==|!=)"
+val = "(-?\d+)"
+regex_string = "\s*".join((var,op,val,iff,var,compre,val))
+regex = re.compile(regex_string)
+
+
+def mod_op(env, var, op, val):
+    v = env[var]
+    if op == "dec":
+        env[var] -= val
+    elif op == "inc":
+        env[var] += val
+    return env
+
+
+def comp_op(env, var, c_op, val):
+    v = env[var]
+    return eval(str(v) + c_op + val)
+
+def parse_line(line):
+    return regex.match(line).groups()
+    
+
+if __name__ == '__main__':
+    data = []
+    env = defaultdict(int)
+    with open("input/day8.txt") as f:
+        for line in f:
+            v_var, v_op, v_val, c_var, c_op, c_val = parse_line(line)
+            if comp_op(env, c_var, c_op, c_val):
+                mod_op(env, v_var, v_op, int(v_val))
+
+    print("Largest value is:", max(env.values()))

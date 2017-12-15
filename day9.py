ddc77c56d2fd33f55715e700f61ec2931ad437e6
Erik Söderberg <Zednosius@gmail.com>
2017-12-15 21:48:34 +0100

    Add solution to day9 part 2

--- day9.py
+++ day9.py
@@ -24,26 +24,33 @@ def lex(data):
 
 
 class Parser:
-    def __init__(self):
+    def __init__(self,dbgprint=False):
         self.i = 0
         self.group = 0
         self.score = 0
         self.garbage = False
         self.parse_table = defaultdict
+        self.garbage_chars = 0
+        self.dbgprint = dbgprint
 
     def inc(self, n=1):
         self.i += n
+        if self.garbage:
+            self.garbage_chars += 1
+
+    def skip(self, n=1):
+        self.i += n
 
     def bang(self):
-        self.inc(2) #Skip the ! and next char
+        self.skip(2) #Skip the ! and next char
     
     def lt(self):
         self.inc()
         self.garbage = True
 
     def gt(self):
-        self.inc()
         self.garbage = False
+        self.inc()
     
     def rbrace(self):
         self.inc()
@@ -57,8 +64,9 @@ class Parser:
 
     def parse(self, lexed):
         while self.i < len(lexed):
-            # print(self.i)
-            # print("Current Token: {}/{}  {}".format(self.i+1, len(lexed), lexed[self.i]))
+            if self.dbgprint:
+                print("Current Token: {}/{}  {} {}".format(self.i+1, len(lexed), lexed[self.i], self.garbage if "garbage" else ""))
+
             if not self.garbage:
                 defaultdict(lambda: self.inc,{
                     Token.LT: self.lt,
@@ -74,7 +82,7 @@ class Parser:
                      Token.GT: self.gt,
                      Token.BANG: self.bang})[lexed[self.i]]()
 
-        return self.score
+        return self
 
 
 def lex_and_parse(string):
@@ -93,13 +101,15 @@ if __name__ == '__main__':
     # {{<ab>},{<ab>},{<ab>},{<ab>}}, score of 1 + 2 + 2 + 2 + 2 = 9.
     # {{<!!>},{<!!>},{<!!>},{<!!>}}, score of 1 + 2 + 2 + 2 + 2 = 9.
     # {{<a!>},{<a!>},{<a!>},{<ab>}}, score of 1 + 2 = 3.
-    print(1 , "==", lex_and_parse("{}"))
-    print(6 , "==", lex_and_parse("{{{}}}"))
-    print(16, "==", lex_and_parse("{{{},{},{{}}}}"))
-    print(1 , "==", lex_and_parse("{<a>,<a>,<a>,<a>}"))
-    print(9 , "==", lex_and_parse("{{<ab>},{<ab>},{<ab>},{<ab>}}"))
-    print(9 , "==", lex_and_parse("{{<!!>},{<!!>},{<!!>},{<!!>}}"))
-    print(3 , "==", lex_and_parse("{{<a!>},{<a!>},{<a!>},{<ab>}}"))
+    print(1 , "==", lex_and_parse("{}").score)
+    print(6 , "==", lex_and_parse("{{{}}}").score)
+    print(16, "==", lex_and_parse("{{{},{},{{}}}}").score)
+    print(1 , "==", lex_and_parse("{<a>,<a>,<a>,<a>}").score)
+    print(9 , "==", lex_and_parse("{{<ab>},{<ab>},{<ab>},{<ab>}}").score)
+    print(9 , "==", lex_and_parse("{{<!!>},{<!!>},{<!!>},{<!!>}}").score)
+    print(3 , "==", lex_and_parse("{{<a!>},{<a!>},{<a!>},{<ab>}}").score)
+    print("10 ==" , Parser(True).parse(lex("<{o\"i!a,<{i<a>")).garbage_chars)
 
 
-    print(parser.parse(lexed))
+    print("Task 1 score: ",parser.parse(lexed).score)
+    print("Garbage Chars:", parser.garbage_chars)
